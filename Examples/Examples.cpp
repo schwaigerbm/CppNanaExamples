330f4f026206bb3991569507442ec5ec0cf7e7dc
bernhard.schwaiger <bernhard.schwaiger@htl-saalfelden.at>
2021-01-12 10:20:37 +0100

    Projektdateien hinzufügen.

---
+++ Examples/Examples.cpp
@@ -0,0 +1,12 @@
+﻿// Examples.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "Examples.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
